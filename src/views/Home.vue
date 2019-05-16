81fa463e2731decfb8c25e84fedbe1d790152b68
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-16 19:37:59 -0400

    Add avatar image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="avatar image"
+             id="avatar-image"
+             src="https://avatars3.githubusercontent.com/u/41254238?s=460&v=4">
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    export default {
+        name: 'home',
+        data() {
+            return {
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+            }
+        },
+        components: {
+        }
+    }
 </script>
+
+<style scoped>
+    #avatar-image {
+        width: 10rem;
+        border-radius: 10%;
+        filter: drop-shadow(0px 16px 16px rgba(130, 130, 137, 0.37));
+    }
+
+</style>
