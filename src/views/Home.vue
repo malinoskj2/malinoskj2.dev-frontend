535ee5029148d20dac31e0c8a784844b5f0418fc
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-16 19:46:59 -0400

    add flex container

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,10 @@
         <img alt="avatar image"
              id="avatar-image"
              src="https://avatars3.githubusercontent.com/u/41254238?s=460&v=4">
+
+        <div id="media-link-container">
+
+        </div>
     </div>
 </template>
 
@@ -25,6 +29,12 @@
         width: 10rem;
         border-radius: 10%;
         filter: drop-shadow(0px 16px 16px rgba(130, 130, 137, 0.37));
+    }
+
+    #media-link-container {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
     }
 
 </style>
