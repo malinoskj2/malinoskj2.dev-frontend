b9b34309fd0c01701cafdc307d6fd4be9cc6b4f1
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-16 20:09:42 -0400

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app" class="patterned-bg">
+        <router-view/>
+    </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  padding-top: 4rem;
-}
+    @import url('https://fonts.googleapis.com/css?family=Fira+Mono:500&display=swap');
+
+    #app {
+        font-family: 'Fira Mono', monospace;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        padding-top: 4rem;
+    }
+
+    html {
+        background-color: rgba(251, 251, 251, 0.59);
+    }
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="home">
-        <img alt="avatar image"
-             id="avatar-image"
+        <img alt="avatar image" id="avatar-image" class="slide-in-blurred-bottom"
              src="https://avatars3.githubusercontent.com/u/41254238?s=460&v=4">
 
         <div id="media-link-container">
-            <font-awesome-icon :icon="['fab', 'github']" size="2x" class="media-link"/>
-            <font-awesome-icon :icon="['fab', 'gitlab']" size="2x" class="media-link"/>
-            <font-awesome-icon :icon="['fas', 'envelope']" size="2x" class="media-link"/>
+            <a v-for="(linkItem, index) in this.mediaLinks" :href="linkItem.link" :key="index" class="fade-in">
+                <font-awesome-icon :icon="linkItem.iconSpecs" size="2x" class="media-link"/>
+                <p>{{ linkItem.name}}</p>
+            </a>
         </div>
+
     </div>
 </template>
 
@@ -18,10 +19,18 @@
         name: 'home',
         data() {
             return {
-
+                mediaLinks: [
+                    {name: 'github', link: 'https://github.com/malinoskj2', iconSpecs: ['fab', 'github']},
+                    {name: 'gitlab', link: 'https://gitlab.com/malinoskj2', iconSpecs: ['fab', 'gitlab']},
+                    {name: 'email', link: "mailto:jesse@malinoskj2.dev", iconSpecs: ['fas', 'envelope']}
+                ],
             }
         },
-        components: {
+        components: {},
+        mounted() {
+            setTimeout(() => {
+
+            }, 2700)
         }
     }
 </script>
@@ -30,7 +39,6 @@
     #avatar-image {
         width: 10rem;
         border-radius: 10%;
-        filter: drop-shadow(0px 16px 16px rgba(130, 130, 137, 0.37));
     }
 
     #media-link-container {
@@ -43,7 +51,64 @@
     }
 
     .media-link {
-        margin: 0rem 1rem 0rem 1rem;
+        color: darkslategray;
+        margin: 0rem 2rem 0rem 2rem;
+        transform: scale(1);
+        transition: transform 130ms ease-in-out;
+    }
+
+    .media-link:hover {
+        color: rgba(47, 79, 79, 0.67);
+
+        cursor: pointer;
+    }
+
+    a {
+        color: inherit;
+
+        text-decoration: none;
+    }
+
+    a p {
+        visibility: hidden;
+        text-decoration: none;
+    }
+
+    a:hover p{
+        visibility: visible;
+        text-decoration: none;
+    }
+
+    .slide-in-blurred-bottom {
+        animation: slide-in-blurred-bottom 0.5s cubic-bezier(0.230, 1.000, 0.30, 1.000) both;
+    }
+
+    .fade-in {
+        animation: fade-in 1.2s 0.5s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+    }
+
+    @keyframes slide-in-blurred-bottom {
+        0% {
+            transform: translateY(300px) scaleY(2.5) scaleX(0.2);
+            transform-origin: 50% 100%;
+            filter: blur(40px) drop-shadow(0px -116px 2px rgba(130, 130, 137, 0.37));
+            opacity: 0;
+        }
+        100% {
+            transform: translateY(0) scaleY(1) scaleX(1);
+            transform-origin: 50% 50%;
+            filter: blur(0) drop-shadow(0px 16px 16px rgba(130, 130, 137, 0.37));
+            opacity: 1;
+        }
+    }
+
+    @keyframes fade-in {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
     }
 
 </style>
