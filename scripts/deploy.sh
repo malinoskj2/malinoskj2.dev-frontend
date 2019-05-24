800e497d79c3cce6a1f90f84b79f7947aea1e6dd
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-24 16:45:55 -0400

    get ec2 ip from env var

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ chmod 600 ./id_rsa
 ssh-add ./id_rsa
 
 # Clear old files
-ssh -i ./id_rsa -p 34 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" travis@malinoskj2.dev rm -r /www/malinoskj2.dev/*
+ssh -i ./id_rsa -p 34 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" travis@${ec2_ip} rm -r /www/malinoskj2.dev/*
 
 # Copy new dist files to be served
-scp -i ./id_rsa -P 34 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -r ${TRAVIS_BUILD_DIR}/dist/* travis@malinoskj2.dev:/www/malinoskj2.dev/
+scp -i ./id_rsa -P 34 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -r ${TRAVIS_BUILD_DIR}/dist/* travis@${ec2_ip}:/www/malinoskj2.dev/
