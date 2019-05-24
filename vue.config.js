aafd4d83f87651212d9873c3ac1f5e03513cead0
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-24 18:13:58 -0400

    setup webpack config better

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,39 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-	configureWebpack: { plugins: [ new BundleAnalyzerPlugin({generateStatsFile: true, openAnalyzer: false, analyzerMode: 'disabled' }) ],
- optimization: {
-  splitChunks: {
-    chunks: 'all'
-  }
-},
-  },
- devServer: {
-    port: 8081,
-    watchOptions: {
-      poll: true,
-    },
-  },
+module.exports = () => {
+    const devServer = {
+        port: 8081,
+        watchOptions: {
+            poll: true,
+        },
+    };
+
+    let configureWebpack = {};
+
+    switch (process.env.NODE_ENV) {
+        case 'development':
+            break;
+        case 'production': {
+            configureWebpack = {
+                plugins: [new BundleAnalyzerPlugin({
+                    generateStatsFile: true,
+                    openAnalyzer: false,
+                    analyzerMode: 'disabled'
+                })],
+                optimization: {
+                    splitChunks: {
+                        chunks: 'all'
+                    }
+                },
+            }
+        }
+        break;
+        default:
+            console.log('unknown NODE_ENV');
+    }
+
+    return  {
+        configureWebpack,
+        devServer
+    }
 };
