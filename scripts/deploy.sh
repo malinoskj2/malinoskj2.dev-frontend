225bb6a4179924642af6d5237be33030dc6746cb
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-24 17:14:03 -0400

    invalidate cloudfront cache when code is pushed to master and builds successfully

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,3 +13,11 @@ ssh -i ./id_rsa -p 34 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyCheckin
 
 # Copy new dist files to be served
 scp -i ./id_rsa -P 34 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -r ${TRAVIS_BUILD_DIR}/dist/* travis@${ec2_ip}:/www/malinoskj2.dev/
+
+aws configure set aws_access_key_id $travis_cloudfront_key
+aws configure set aws_secret_access_key $travis_cloudfront_secret
+aws configure set preview.cloudfront true
+
+aws cloudfront create-invalidation \
+--distribution-id ${cloudfront_id} \
+--paths "/*" >/dev/null 2>&1 
