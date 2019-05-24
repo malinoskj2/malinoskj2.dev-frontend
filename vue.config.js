e17e55b5a819b6c88ba86ab7913d97dc199a7d76
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-24 17:48:46 -0400

    add dep to analyze bundle size

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
+	configureWebpack: { plugins: [ new BundleAnalyzerPlugin({generateStatsFile: true, openAnalyzer: false, analyzerMode: 'disabled' }) ],
+ optimization: {
+  splitChunks: {
+    chunks: 'all'
+  }
+},
+  },
  devServer: {
     port: 8081,
     watchOptions: {
