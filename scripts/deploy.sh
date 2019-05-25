7f0ef05028d2b3b0a1718362ee21b1921a81bea5
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-25 10:13:05 -0400

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-npm run config-creds
-npm run deploy
+npm install -g serverless && 
+npm run config-creds &&
+echo "Y" | npm run deploy 
 
-aws configure set aws_access_key_id $travis_cloudfront_key
-aws configure set aws_secret_access_key $travis_cloudfront_secret
+aws configure set aws_access_key_id $travis_cloudfront_key &&
+aws configure set aws_secret_access_key $travis_cloudfront_secret &&
 aws configure set preview.cloudfront true
 
 aws cloudfront create-invalidation \
