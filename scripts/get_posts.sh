ced946082d7601b1da95d40a0f09470b486ddbae
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-25 16:58:21 -0400

    render pages before serving

---
+++ scripts/get_posts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get posts from cms and write to file
+
+endpoint='https://cms.malinoskj2.dev/api/collections/get/posts';
+
+mkdir -p build && 
+curl -X GET $endpoint > build/posts.json
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,24 @@
-module.exports = {
-  pluginOptions: {
+const posts = require('./build/posts.json');
+
+
+const postPaths = posts.entries.map(post => `/posts/${post._id}`);
+
+
+postPaths.forEach(post => console.log(post));
+
+let pluginOptions = {
     prerenderSpa: {
       registry: undefined,
       renderRoutes: [
-        '/',
-        '/404'
+	      '/',
+	      '/404',
+	      ...postPaths
       ],
       useRenderEvent: true,
       headless: true,
       onlyProduction: true
     }
-  }
-}
+};
+
+
+module.exports =  () => { return { pluginOptions }; }
