76071ca4e36039483e4a8a165059bf768443ea70
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-25 09:51:54 -0400

    add serverless

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)"
-
-# Set correct perm
-chmod 600 ./id_rsa
-
-# add to ssh agent
-ssh-add ./id_rsa
-
-# Clear old files
-ssh -i ./id_rsa -p 34 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" travis@${ec2_ip} rm -r /www/malinoskj2.dev/*
-
-# Copy new dist files to be served
-scp -i ./id_rsa -P 34 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -r ${TRAVIS_BUILD_DIR}/dist/* travis@${ec2_ip}:/www/malinoskj2.dev/
+npm run config-creds
+npm run deploy
 
 aws configure set aws_access_key_id $travis_cloudfront_key
 aws configure set aws_secret_access_key $travis_cloudfront_secret
