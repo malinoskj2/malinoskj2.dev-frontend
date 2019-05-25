aa06e2fafb617eebe06ae8cae70527f8f371f1ff
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-25 11:35:06 -0400

    add route guards + redirects

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from './store'
 
 const Home2 = () => import('./views/Home2.vue');
 const PostView = () => import('./components/Post.vue');
@@ -8,7 +10,7 @@ Vue.use(Router);
 
 export default new Router({
     scrollBehavior() {
-        return { x: 0, y: 0 };
+        return {x: 0, y: 0};
     },
     routes: [
         {
@@ -20,6 +22,36 @@ export default new Router({
             path: '/posts/:id',
             name: 'post-view',
             component: PostView,
+            beforeEnter: (to, from, next) => {
+                redirectIfNonexistent(postIdFromPath(to.path), '/', next);
+            }
+        },
+        {
+            path: '*',
+            redirect: { name: 'home' }
         }
     ]
-})
+});
+
+const redirectIfNonexistent = (id, redirectPath, next) => {
+    if (!storeContainsPost(id)) {
+        store.dispatch('initPosts')
+            .then(res => {
+                if (storeContainsPost(id)) {
+                    next();
+                } else {
+                    next({path: redirectPath});
+                }
+            })
+            .catch(() => console.log('failed to dispatch'));
+    }
+    next();
+};
+
+const storeContainsPost = (postId) => {
+    return store.getters.posts.some(post => post._id == postId);
+};
+
+const postIdFromPath = (path) => {
+    return path.substring(path.lastIndexOf('/') + 1);
+};
