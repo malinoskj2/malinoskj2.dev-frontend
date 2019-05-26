f907a9f3e397c295901d41336d16d780ea51d5cc
Jesse Malinosky <jesse@malinoskj2.dev>
2019-05-26 19:46:48 -0400

    fix vuex plugins

--- src/store/store.js
+++ src/store/store.js
@@ -5,18 +5,16 @@ import dayjs from 'dayjs'
 
 import postModule from './mod/posts.js'
 
-const persistent = ['postModule'];
-
 const vuexLocal = new VuexPersistence({
     key: 'j2',
     storage: window.localStorage,
-    modules: process.env.NODE_ENV === 'production' ? persistent : []
+    modules: ['postModule']
 });
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    plugins: [vuexLocal.plugin],
+    plugins: process.env.NODE_ENV === 'production' ? [vuexLocal.plugin] : [],
     state: {
         sessionStartTime: dayjs(),
     },
