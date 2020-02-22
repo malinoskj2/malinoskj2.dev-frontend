24bb8f0f15697a6ea98ba7273324017f7febc993
Jesse Malinosky <jesse@malinoskj2.dev>
2020-02-22 17:35:46 -0500

    add markdown loader

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
-const devServer = {
-    port: 8081,
-    watchOptions: {
-        poll: true,
-    },
-};
-
-module.exports = () => {
-    return {devServer};
-};
+module.exports = {
+    chainWebpack(config){
+        config.module.rule('md')
+            .test(/\.md/)
+            .use('vue-loader')
+            .loader('vue-loader')
+            .end()
+            .use('vue-markdown-loader')
+            .loader('vue-markdown-loader/lib/markdown-compiler')
+            .options({
+                raw: true
+            })
+    }
+}
